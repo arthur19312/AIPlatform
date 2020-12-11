d6ee7c4e7ec8ae0c8183d1929262ce69b0bde966
韩紫嫣 <blodcell@163.com>
2020-12-11 21:12:21 +0800

    首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,14 +2,18 @@
 	<view class="container">
 		<!-- logo / 标题 -->
 		<view class="topbar">
-			<text>智慧云创</text>
-			<text>💌</text>
+			<view>
+				<image src="../../static/logo/1.png" style="width: 320rpx;height: 60rpx;"></image>
+			</view>
+			<view>
+				<image src="../../static/iconfont/message.png" style="width: 60rpx;height: 60rpx;position: relative;top: 10rpx;"></image>
+			</view>
 		</view>
 
 		<!-- 展示栏 -->
 		<view class="content">
 
-			<u-swiper :list="list" mode="round" indicator-pos="bottomCenter" effect3d="true" interval="3000" height="800"></u-swiper>
+			<u-swiper :list="list" title="true" mode="round" indicator-pos="bottomCenter" effect3d="true" interval="3000" height="800"></u-swiper>
 
 		</view>
 
@@ -23,43 +27,39 @@
 				title: 'Hello',
 				list: [
 					{
-						image: '../../static/image/1.jpg',
+						image: '../../../static/image/1.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/2.jpg',
+						image: '../../../static/image/2.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/3.jpg',
+						image: '../../../static/image/3.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/4.jpg',
+						image: '../../../static/image/4.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/5.jpg',
+						image: '../../../static/image/5.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/6.jpg',
+						image: '../../../static/image/6.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/7.jpg',
+						image: '../../../static/image/7.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/8.jpg',
+						image: '../../../static/image/8.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					},
 					{
-						image: '../../static/image/9.jpg',
-						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
-					},
-					{
-						image: '../../static/image/10.jpg',
+						image: '../../../static/image/9.jpg',
 						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
 					}
 					
@@ -84,16 +84,16 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: space-between;
-		height: 300rpx;
-		line-height: 300rpx;
+		height: 80rpx;
+		line-height: 80rpx;
 		font-size: 40rpx;
 		font-weight: bold;
-		
+		margin-top: 10rpx;
 		padding: 0 40rpx;
 	}
 
 	.content {
-		margin-top: 60rpx;
+		margin-top: 40rpx;
 	}
 
 	.tab-cur {}
---
+++ pages/puzzle/index.vue
@@ -0,0 +1,106 @@
+<template>
+	<view class="container">
+		<!-- logo / 标题 -->
+		<view class="topbar">
+			<text>回忆拼图</text>
+			<view>
+				<image src="../../static/iconfont/tip.png" style=""></image>
+			</view>
+		</view>
+
+		<!-- 展示栏 -->
+		<view class="content">
+
+			<u-swiper :list="list" title="true" mode="round" indicator-pos="bottomCenter" effect3d="true" interval="3000" height="800"></u-swiper>
+
+		</view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				list: [
+					{
+						image: '../../static/image/1.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/2.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/3.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/4.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/5.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/6.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/7.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/8.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					},
+					{
+						image: '../../static/image/9.jpg',
+						title: '昨夜星辰昨夜风，画楼西畔桂堂东'
+					} 
+					
+				],
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		margin: 0;
+	}
+
+	.topbar {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		height: 300rpx;
+		line-height: 300rpx;
+		font-size: 40rpx;
+		font-weight: bold;
+		
+		padding: 0 40rpx;
+	}
+
+	.content {
+		margin-top: 60rpx;
+	}
+
+	.tab-cur {}
+
+	.tab {}
+
+	.tab-img {
+		width: 400rpx;
+		height: 400rpx;
+		border-radius: 6px;
+	}
+</style>
